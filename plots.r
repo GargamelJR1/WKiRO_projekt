33df4091c15aeeddd6c9dc77691947867906dd50
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2025-04-26 00:13:20 +0200

    improved plot CI function

--- plots.r
+++ plots.r
@@ -1,12 +1,13 @@
-plot_confidence_intervals <- function(bc_pairs, labels, n, alpha, ci_fun) {
-  # Compute confidence intervals for each pair
-  ci_mat <- t(sapply(bc_pairs, function(x) ci_fun(x[1], x[2], n, alpha)))
+plot_confidence_intervals <- function(ci_mat, labels, ci_holm_mat = NULL) {
   mids <- rowMeans(ci_mat)
+
+  # Combine for ylim if holm_mat is provided
+  ylim_range <- if (!is.null(ci_holm_mat)) range(c(ci_mat, ci_holm_mat)) else range(ci_mat)
 
   # Set up plot
   plot(
     seq_along(labels), mids,
-    ylim = range(ci_mat),
+    ylim = ylim_range,
     xaxt = "n", xlab = "", ylab = "Confidence Interval",
     pch = 19, main = "Pair-wise comparison of models (i,j)", type = "n"
   )
@@ -15,13 +16,27 @@ plot_confidence_intervals <- function(bc_pairs, labels, n, alpha, ci_fun) {
   # horizontal line at y = 0
   abline(h = 0, lty = 2)
 
+  # Add x offset if Holm's intervals are provided
+  offset <- if (is.null(ci_holm_mat)) 0 else 0.15
+
+  dot_size <- 1.8
+  line_width <- 2.2
+
   for (i in seq_along(labels)) {
     # Vertical line
-    lines(c(i, i), ci_mat[i,], lwd = 2)
+    lines(c(i - offset, i - offset), ci_mat[i,], lwd = line_width)
     # Horizontal lines at top and bottom
-    segments(i - 0.2, ci_mat[i, 1], i + 0.2, ci_mat[i, 1], lwd = 2)
-    segments(i - 0.2, ci_mat[i, 2], i + 0.2, ci_mat[i, 2], lwd = 2)
+    segments(i - offset - 0.1, ci_mat[i, 1], i - offset + 0.1, ci_mat[i, 1], lwd = line_width)
+    segments(i - offset - 0.1, ci_mat[i, 2], i - offset + 0.1, ci_mat[i, 2], lwd = line_width)
     # Points at the center of the intervals
-    points(i, mids[i], pch = 19, cex = 2)
+    points(i - offset, mids[i], pch = 19, cex = dot_size)
+
+    # If Holm's intervals are provided, plot them as well
+    if (!is.null(ci_holm_mat)) {
+      lines(c(i + offset, i + offset), ci_holm_mat[i,], lwd = line_width)
+      segments(i + offset - 0.1, ci_holm_mat[i, 1], i + offset + 0.1, ci_holm_mat[i, 1], lwd = line_width)
+      segments(i + offset - 0.1, ci_holm_mat[i, 2], i + offset + 0.1, ci_holm_mat[i, 2], lwd = line_width)
+      points(i + offset, rowMeans(ci_holm_mat)[i], pch = 19, cex = dot_size)
+    }
   }
 }
